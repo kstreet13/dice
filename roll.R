8c03b9c03136ea27b7284abc159994a8eb5ca663
Kelly Street <street.kelly@gmail.com>
2023-03-01 15:50:31 -0800

    after first trial

--- roll.R
+++ roll.R
@@ -1,10 +1,10 @@
 
-# it's a function for rolling dice!
+# a function for rolling dice!
 
 roll <- function(times = 1, sides = 6, verbose = TRUE){
-    times <- as.integer(times)
+    suppressWarnings({ times <- as.integer(times) })
     if(is.na(times) || times < 1) stop('"times" argument must be a positive integer.')
-    sides <- as.integer(sides)
+    suppressWarnings({ sides <- as.integer(sides) })
     if(is.na(sides) || sides < 1) stop('"sides" argument must be a positive integer.')
     stopifnot(is.logical(verbose))
     r <- floor(runif(times, max = sides)+1)
